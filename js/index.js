b905a6ac6f0f0e8d8f5611919f87984ed61c07e8
Evelyn <evelyn.chavez92@yahoo.com>
2021-11-10 12:30:07 -0600

    add handlePageLoaded

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const goalsInput = () => document.querySelector("#goals-input")
+const goalsUl = () => document.querySelector("#goals-list")
+
+
+
+const handlePageLoaded = () => {
+
+}
+
+document.addEventListener("DOMContentLoaded", handlePageLoaded)
+
