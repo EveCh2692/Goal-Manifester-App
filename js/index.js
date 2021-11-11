a16e24e6c216cc8c59cdbdb874ee2d8f8d7ae258
Evelyn <evelyn.chavez92@yahoo.com>
2021-11-10 21:06:09 -0600

    add new input to to html, updated db.jason api, submit event

--- js/index.js
+++ js/index.js
@@ -1,10 +1,35 @@
-const goalsInput = () => document.querySelector("#goals-input")
+const goalsForm = () => document.querySelector("#create-goals form")
 const goalsUl = () => document.querySelector("#goals-list")
 
 
+const handleSubmit = (e) => {
+e.preventDefault()
+const goalType = e.target[0].value
+const goalDeadline = e.target[1].value
+const goalTitle = e.target[2].value
+const confiObject = {
+    method:"POST",
+    headers: {
+        'Content-Type': 'application/json'
+        
+      },
+     body: JSON.stringify({
+        "type": goalType,
+        "title": goalTitle,
+        "completed": false,
+        "deadline": goalDeadline,
+        "shortTerm": []
+     })
+    }
+  fetch("http://localhost:3000/longTermGoals", confiObject)
+    .then(response => response.json() )
+    .then(goalObject => console.log(goalObject))
+
+}
+
 
 const handlePageLoaded = () => {
-
+goalsForm().addEventListener("submit", handleSubmit )
 }
 
 document.addEventListener("DOMContentLoaded", handlePageLoaded)
